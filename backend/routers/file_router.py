6e123752e84a429ca09d605986d4b9dfde605bf6
Georgi Dodekov <g.dodekov@gmail.com>
2024-04-03 16:38:27 +0300

    Refactoring existing functionality

--- backend/routers/file_router.py
+++ backend/routers/file_router.py
@@ -1,6 +1,6 @@
 import os
 from backend.services.image_service import transform_to_pencil_sketch, allowed_file
-from backend.services.file_service import save_uploaded_file, save_sketch, delete_file, create_folders
+from backend.services.file_service import save_uploaded_file, save_sketch, delete_file, create_folders, get_file_path
 from flask import Blueprint, request, jsonify, render_template, redirect
 
 file_router = Blueprint('file', __name__)
@@ -26,8 +26,8 @@ def upload_file():
 
     folder = create_folders()
 
+    # Save the uploaded file
     if file and allowed_file(file.filename):
-        # Save the uploaded file
         file_path = save_uploaded_file(file)
 
         return redirect(request.referrer)
@@ -43,15 +43,17 @@ def delete_uploaded_file(filename):
 @file_router.route('/sketch', methods=['GET', 'POST'])
 def sketch_file():
 
-    upload_dir = 'uploads'
-    uploaded_files = os.listdir(upload_dir)
+    # upload_dir = 'uploads'
+    # uploaded_files = os.listdir(upload_dir)
+    #
+    # # Assuming there's only one file uploaded, you can take the first file from the list
+    # if uploaded_files:
+    #     file_name = uploaded_files[0]
+    #     file_path = os.path.join(upload_dir, file_name)
+    # else:
+    #     return jsonify({"error": "No files uploaded"})
 
-    # Assuming there's only one file uploaded, you can take the first file from the list
-    if uploaded_files:
-        file_name = uploaded_files[0]
-        file_path = os.path.join(upload_dir, file_name)
-    else:
-        return jsonify({"error": "No files uploaded"})
+    file_path = get_file_path()
 
     # Transform the image to a pencil sketch
     sketch = transform_to_pencil_sketch(file_path)
@@ -59,4 +61,7 @@ def sketch_file():
     # Save the sketch
     sketch_path = save_sketch(sketch)
 
-    return render_template('result.html', original_file=file_path, sketch_file=sketch_path)
+    absolute_original_path = os.path.abspath(file_path)
+    absolute_sketch_path = os.path.abspath(sketch_path)
+
+    return render_template('result.html', original_file=absolute_original_path, sketch_file=absolute_sketch_path)
